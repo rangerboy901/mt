10ae4af7ea80b1b22175798ea7b912509b520f75
Joseph DeWeese <rangerboy901@icloud.com>
2022-07-04 09:05:52 -0500

    Initial Commit

---
+++ mt/mtApp.swift
@@ -0,0 +1,17 @@
+//
+//  mtApp.swift
+//  mt
+//
+//  Created by Joseph DeWeese on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct mtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
